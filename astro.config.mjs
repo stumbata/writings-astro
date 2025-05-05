7a3468636a58aad3bd6b3accc19a9465e47b3a68
Stanislav Ognyanov <s.ognyanov@astoundcommerce.com>
2025-05-05 14:41:05 +0300

    add google fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    experimental: {
+        fonts: [
+            {
+                provider: fontProviders.google(),
+                name: 'Lora',
+                cssVariable: '--font-lora',
+                weights: ['400', '700'],
+            },
+        ],
+    },
 });
